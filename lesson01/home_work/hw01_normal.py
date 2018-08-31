6324a513d4388777cde96dbbcd27242bfd757598
usermuser <lwrdrstv@gmail.com>
2018-08-31 13:36:52 +0500

    solved with using for cycle

--- lesson01/home_work/hw01_normal.py
+++ lesson01/home_work/hw01_normal.py
@@ -9,7 +9,15 @@ __author__ = 'Хусаинов Рустам Азатович'
 # Подсказки:
 # * постарайтесь решить задачу с применением арифметики и цикла while;
 # * при желании и понимании решите задачу с применением цикла for.
-user_input = input('Введите целое беззнаковое число, например 12345. ')
+user_input = input('Введите целое беззнаковое число, например 12345.\n\
+Задача решена с помощью цикла while. ')
+
+'''
+This version of program i tried to solve using while operator
+Checking that user input is correct not implemented since we assume that
+user will enter correct type of number
+'''
+
 user_input_list = list(user_input)
 input_length = len(user_input_list)
 i=0
@@ -20,8 +28,20 @@ while i < input_length:
         i+=1
     else:
         i+=1
-print('max digit in engered number is', max)
-    
+print('max digit in entered number is', max)
+
+
+user_input = input('Введите целове баззнаковое число, например 23213.\n\
+Задача решена с применением цикла for. ')
+user_input_list = list(user_input)
+input_length = len(user_input_list)
+for i in range(input_length):
+    if int(user_input_list[i]) > int(max):
+        max = user_input_list[i]
+        i+=1
+    else:
+        i+=1
+print('Максимальное число из введенных цифр ', max)
 
 # Задача-2: Исходные значения двух переменных запросить у пользователя.
 # Поменять значения переменных местами. Вывести новые значения на экран.
@@ -36,4 +56,4 @@ print('max digit in engered number is', max)
 # Коэффициенты уравнения вводятся пользователем.
 # Для вычисления квадратного корня воспользуйтесь функцией sqrt() модуля math:
 # import math
-# math.sqrt(4) - вычисляет корень числа 4
+# math.sqrt(4) - вычисляет корень числа 4.
